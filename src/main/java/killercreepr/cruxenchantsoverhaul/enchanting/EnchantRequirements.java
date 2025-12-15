f887887c3447c84f49630f7292063daf45e3cda5
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-12-15 13:21:09 -0500

    ayo

--- src/main/java/killercreepr/cruxenchantsoverhaul/enchanting/EnchantRequirements.java
+++ src/main/java/killercreepr/cruxenchantsoverhaul/enchanting/EnchantRequirements.java
@@ -11,7 +11,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class EnchantRequirements {
